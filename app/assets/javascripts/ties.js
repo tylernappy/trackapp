121d97c71bbaa9d322f6616d339eedfe7e847a71
Tyler Nappy <tylernappy@gmail.com>
2014-04-08 18:47:25 -0400

    added javascript to test for correct input of ties

---
+++ app/assets/javascripts/ties.js
@@ -0,0 +1,33 @@
+submitForms = function() {
+  //see what areas are filled in and what are not
+  var places = [];
+  var rows = document.getElementsByClassName("places-table")[0].children[0].children[0].children
+  for (i=3; i<(rows.length-1); i++) {
+    var rowElements = rows[i].children;
+    index = i-3;
+    places[index] = 0;
+    for (j=1; j<rowElements.length; j++) {
+      var schoolId = rowElements[j].children[0].value; //This will be '0' for nobody, and the school id for anyting else
+      if (schoolId != 0) {
+        places[index] += 1;
+      }
+
+    }
+  }
+
+  //determine if ties make sense depending on how user input results
+  for (i=0; i<(places.length-1); i++) {
+    if (places[i]>1) {
+      for (j=i+1; j<=((i+1)+(places[i]-2)); j++) {
+        if (places[j] != 0) {
+          alert("These ties do not make sense. Try relooking at it");
+          return
+        }
+      }
+    }
+  }
+
+  //find button used to submit form created by rails; and click this button
+  button = document.getElementsByClassName('rails-btn')[0];
+  button.click();
+}
